52be7ca805a58fdadef44b5171a313bcf96676bb
Vocab App User <user@example.com>
2026-01-29 18:45:52 +0900

    fix: remove invalid stripe apiVersion to fix internal server error

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -5,7 +5,7 @@ import { prisma } from "@/app/lib/prisma";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: "2025-12-15.clover",
+    // apiVersion: "2025-12-15.clover",
 });
 
 export async function POST(req: Request) {
